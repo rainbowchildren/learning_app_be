3f6e6449163bfd18a21bd8053991451702440e3e
Nikhil Satya Sai <nikhil@Nikhils-MacBook-Pro.local>
2025-10-02 18:13:02 +0530

    added hwt middle ware for the userProgress service

--- services/userProgressService.js
+++ services/userProgressService.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { validateAnswer } from "../controllers/userProgressController.js";
+import { ROLES } from "../constants/constants.js";
+import { verifyJWTMiddleware } from "../middlewares/authMiddleware.js";
 
 const userProgress = Router();
 
-userProgress.patch("/validate/:questionId", validateAnswer);
+userProgress.patch(
+  "/validate/:questionId",
+  verifyJWTMiddleware,
+  authorizeRoles(ROLES.STUDENT),
+  validateAnswer
+);
 
 export default userProgress;
