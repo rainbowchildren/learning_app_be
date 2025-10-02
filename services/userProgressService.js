0aa24118e341a4b6e9a1ed0fd041c3e680c29ad8
Nikhil Satya Sai <nikhil@Nikhils-MacBook-Pro.local>
2025-10-02 18:13:36 +0530

    import issue fixed

--- services/userProgressService.js
+++ services/userProgressService.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { validateAnswer } from "../controllers/userProgressController.js";
 import { ROLES } from "../constants/constants.js";
 import { verifyJWTMiddleware } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/RBACMiddleware.js";
 
 const userProgress = Router();
 
