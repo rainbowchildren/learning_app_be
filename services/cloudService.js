1ad5c98a6cdcfd6bd4dd7ac73eeda49b6290cd67
Nikhil Satya Sai <reading.rainbowchildren@gmail.com>
2026-03-01 17:13:11 +0530

    fixed, removed debug logs

--- services/cloudService.js
+++ services/cloudService.js
@@ -18,9 +18,7 @@ cloudService.post("/upload", upload.single("file"), uploadS3);
 cloudService.get("/media/:type/:fileName", async (req, res) => {
   try {
     const { type, fileName } = req.params;
-    console.log("_DEBUG 1", { type, fileName });
     const key = `${type}/${fileName}`;
-    console.log("_DEBUG 2", key);
     // Check file exists in S3
     try {
       await s3.send(
@@ -31,14 +29,6 @@ cloudService.get("/media/:type/:fileName", async (req, res) => {
       );
     } catch (err) {
       console.log(err);
-      console.log("_DEBUG 3", err);
-      console.log(
-        "FROM S3",
-        process.env.AWS_ACCESS_KEY_ID,
-        process.env.AWS_SECRET_ACCESS_KEY,
-        process.env.AWS_BUCKET_NAME,
-        process.env.AWS_REGION,
-      );
       return res
         .status(404)
         .json({ error: "File not found in S3", err: err.toString() });
